ad9b234c6642d0acb75f4b5b92136e061fddf9b0
Loilo <florian.reuschel@gmx.de>
2018-03-23 12:41:27 +0100

    Add source files

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 const { VueLoaderPlugin } = require('vue-loader')
+const { resolve } = require('path')
 
 module.exports = {
+  entry: './src/EmptyComponent.vue',
   module: {
     rules: [
       {
         test: /\.vue$/,
         loader: 'vue-loader',
         include: [
-          '/any/path/really'
+          resolve(__dirname, 'src')
         ]
       }
     ]
